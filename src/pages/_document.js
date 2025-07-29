4ba793f0710b630148fe37c5a55ded7cef5de226
nhan14t1 <nhan.14t1@gmail.com>
2025-07-29 10:45:30 +0700

    Update metadata

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Nhân Phan Quí - Nevel Tech test</title>
+        <link rel='icon' href='/images/white-logo.svg' />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
