2341c3d8a3debdc0bb846ed61220dade89314cc6
nhan14t1 <nhan.14t1@gmail.com>
2025-07-29 10:54:36 +0700

    update language to vi

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="vi">
       <Head>
         <title>Nhân Phan Quí - Nevel Tech test</title>
         <link rel='icon' href='/images/white-logo.svg' />
